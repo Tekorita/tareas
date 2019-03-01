bc6e57995ec82867d6785567d0a2c5bd3620e95e
DavidPalma <davidpalmalugo@gmail.com>
2019-03-01 01:54:56 -0430

    creamos el primer componente de prueba como nav

--- src/App.js
+++ src/App.js
@@ -6,20 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+          <nav>
+          
+          </nav>  
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h3>TITULO CON BOOTSTRAP 4</h3>
       </div>
     );
   }
