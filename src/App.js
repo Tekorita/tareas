07ae7585fe1c22d558cfb12d36cab4ea3e8e1be7
DavidPalma <davidpalmalugo@gmail.com>
2019-03-01 02:22:06 -0430

    alteramos propiedades de los componentes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Navigation from './components/Navigation';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <nav>
-          
-          </nav>  
+          <Navigation titulo="Mi primera navegacion"/>
+          <Navigation titulo="Mi Segunda navegacion"/>
+          <Navigation titulo="Mi Tercera navegacion"/>
           <img src={logo} className="App-logo" alt="logo" />
           <h3>TITULO CON BOOTSTRAP 4</h3>
       </div>
