8a901f8638f2a1f4850ed590fbdbf5ccbac0098d
DavidPalma <davidpalmalugo@gmail.com>
2019-03-04 03:27:39 -0430

    Se agrego componente para agregar tareas y funcion para eliminar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
+import TodoForm from './components/TodoForm';
 import { todos } from './todos.json';
 
 class App extends Component {
@@ -9,13 +10,31 @@ class App extends Component {
     super();
     this.state = {
       todos
+    };
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+    
+  }
+
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  removeTodo(index) {
+    if (window.confirm('Estas seguro de que deseas eliminar la tarea?')){ //Los eventos propios del navegador como confirm o alert para que no den error en react se agregan con la palabra window
+        this.setState({
+        todos: this.state.todos.filter((e, i) => { //la funcion map recorre un arreglo y te devuelve sus datos, en cambio filter recorre un arreglo y muestra los datos si cumplen con una condicion
+          return i !== index
+        })
+      })
     }
   }
 
   render() {
     const tododos = this.state.todos.map((todo, i) => {
       return(
-        <div className="col-md-4">
+        <div className="col-md-4" key={i}>
           <div className="card mt-4 mb-4">
             <div className="card-header">
               <h3>{todo.title}</h3>
@@ -24,19 +43,25 @@ class App extends Component {
             <div className="card-body">
               <p>{todo.description}</p>
               <p><mark>{todo.responsable}</mark></p>
+            </div>
+            <div className="card-footer">
+              <button className="btn btn-danger" onClick={this.removeTodo.bind(this, i)}>
+                Eliminar
+              </button>
             </div>  
           </div>
         </div>  
-      )    
+      );    
     })
 
     return (
       <div className="App">
           <Navigation titulo=<span className="badge badge-pill badge-light ml-2">{this.state.todos.length}</span>/>
           <img src={logo} className="App-logo" alt="logo" />
-          <h3>TITULO CON BOOTSTRAP 4</h3>
+          <h3>Gestor de Tareas</h3>
           <div className="container">
             <div className="row mt-4">
+              <TodoForm onAddTodo={this.handleAddTodo}/>
               { tododos }
             </div>
           </div>
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,2 +1,61 @@
 import React, { Component } from 'react';
 
+class TodoForm extends Component {
+	constructor(){
+		super();
+		this.state = {
+			title: '',
+			responsable: '',
+			description: '',
+			priority: ''
+		};
+		
+		this.handleInput = this.handleInput.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleInput(e) {
+		const { value, name } = e.target;
+		this.setState({
+			[name]: value
+		})
+		console.log(this.state);
+	}
+
+	handleSubmit(e) {
+		e.preventDefault(); //para evitar refrescar la pagina
+		this.props.onAddTodo(this.state);
+		console.log("Enviando los datos...");
+
+	}
+
+	render(){
+		return (
+			<div className="card">
+				<form className="card-body" onSubmit={ this.handleSubmit }>
+					<div className="form-group">
+						<input type="text" name="title" onChange={this.handleInput} className="form-control" placeholder="Titulo"/>
+					</div>
+					<div className="form-group">
+						<input type="text" name="responsable" onChange={this.handleInput} className="form-control" placeholder="Responsable"/>
+					</div>
+					<div className="form-group">
+						<input type="text" name="description" onChange={this.handleInput} className="form-control" placeholder="Descripcion"/>
+					</div>
+					<div className="form-group">
+						<select name="priority" onChange={this.handleInput} className="form-control">
+							<option>low</option>
+							<option>medium</option>	
+							<option>high</option>
+						</select>
+					</div>
+						<button type="submit" className="btn btn-primary">
+						Agregar
+						</button>
+				</form>	
+			</div>
+		)
+	}
+}
+
+export default TodoForm;
